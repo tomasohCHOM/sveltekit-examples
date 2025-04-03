cbfd4be0a8744d5558dd57ba8131795198540f18
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2025-04-03 14:13:07 -0700

    refactor and finalize tictactoe example

--- src/routes/tictactoe/+page.svelte
+++ src/routes/tictactoe/+page.svelte
@@ -1,76 +1,65 @@
 <script lang="ts">
-	// Constant variables
 	const CROSS = "X";
 	const CIRCLE = "O";
+	const PLAYERS = [CROSS, CIRCLE];
 
-	// State variables
-	let board = $state([
-		["", "", ""],
-		["", "", ""],
-		["", "", ""]
-	]);
+	let board = $state(createEmptyBoard());
 	let playerTurn = $state(0);
 	let isGameOver = $state(false);
 	let winner = $state("");
 
-	// Checks if either player has won the game (or if it resulted in a tie)
-	// Does not take any parameters since we are using the board variable directly
-	function checkBoard() {
+	function createEmptyBoard() {
+		return [
+			["", "", ""],
+			["", "", ""],
+			["", "", ""]
+		];
+	}
+
+	function checkWinner() {
 		// Check rows and columns
 		for (let i = 0; i < 3; i++) {
-			if (board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-				isGameOver = true;
-				winner = board[i][0];
-				return;
-			}
-			if (board[0][i] && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
-				isGameOver = true;
-				winner = board[0][i];
-				return;
-			}
+			if (board[i][0] && board[i].every((cell) => cell === board[i][0])) return true;
+			if (
+				board[0][i] &&
+				[board[0][i], board[1][i], board[2][i]].every((cell) => cell === board[0][i])
+			)
+				return true;
 		}
-
 		// Check diagonals
-		if (board[0][0] && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-			isGameOver = true;
-			winner = board[0][0];
-			return;
+		if (
+			board[1][1] &&
+			((board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
+				(board[0][2] === board[1][1] && board[1][1] === board[2][0]))
+		) {
+			return true;
 		}
-		if (board[0][2] && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-			isGameOver = true;
-			winner = board[0][2];
-			return;
-		}
+		return false;
+	}
 
-		// Check for a tie
+	function checkBoardState() {
+		if (checkWinner()) {
+			isGameOver = true;
+			winner = `${playerTurn}`;
+			return;
+		}
 		if (board.flat().every((cell) => cell !== "")) {
 			isGameOver = true;
 			winner = "Tie";
 		}
 	}
 
-	function handleCellClick(rowIndex: number, colIndex: number) {
-		if (board[rowIndex][colIndex] !== "" || isGameOver) {
-			return;
+	function handleCellClick(row: number, col: number) {
+		if (board[row][col] || isGameOver) return;
+		board[row][col] = PLAYERS[playerTurn];
+		checkBoardState();
+		if (!isGameOver) {
+			playerTurn ^= 1;
 		}
-		if (playerTurn === 0) {
-			board[rowIndex][colIndex] = CROSS;
-		} else {
-			board[rowIndex][colIndex] = CIRCLE;
-		}
-		checkBoard();
-		if (isGameOver) {
-			return;
-		}
-		playerTurn ^= 1;
 	}
 
 	function resetGame() {
-		board = [
-			["", "", ""],
-			["", "", ""],
-			["", "", ""]
-		];
+		board = createEmptyBoard();
 		playerTurn = 0;
 		isGameOver = false;
 		winner = "";
@@ -83,32 +72,30 @@
 
 <main>
 	<h1>Tic Tac Toe</h1>
-	<section>
-		<div class="board">
-			{#each board as row, rowIndex}
-				<div class="row">
-					{#each row as cell, colIndex}
-						<button
-							class="cell"
-							class:border-top={rowIndex !== 0}
-							class:border-bottom={rowIndex !== 2}
-							class:border-left={colIndex !== 0}
-							class:border-right={colIndex !== 2}
-							onclick={() => handleCellClick(rowIndex, colIndex)}
-						>
-							{#if cell !== ""}
-								<img
-									src="/tictactoe/{cell === CROSS ? 'cross' : 'circle'}.svg?t=${Date.now()}"
-									alt="{cell} Icon"
-								/>
-							{/if}
-						</button>
-					{/each}
-				</div>
-			{/each}
-		</div>
-	</section>
-	<p class="text-center text-lg">
+	<div class="board">
+		{#each board as row, rowIndex}
+			<div class="row">
+				{#each row as cell, colIndex}
+					<button
+						class="cell"
+						class:border-top={rowIndex !== 0}
+						class:border-bottom={rowIndex !== 2}
+						class:border-left={colIndex !== 0}
+						class:border-right={colIndex !== 2}
+						onclick={() => handleCellClick(rowIndex, colIndex)}
+					>
+						{#if cell !== ""}
+							<img
+								src="/tictactoe/{cell === CROSS ? 'cross' : 'circle'}.svg?t=${Date.now()}"
+								alt="{cell} Icon"
+							/>
+						{/if}
+					</button>
+				{/each}
+			</div>
+		{/each}
+	</div>
+	<p>
 		{#if isGameOver}
 			{#if winner === "Tie"}
 				It's a tie!
@@ -145,6 +132,11 @@
 
 	button {
 		cursor: pointer;
+	}
+
+	p {
+		text-align: center;
+		font-size: 1.125rem;
 	}
 
 	.board {
